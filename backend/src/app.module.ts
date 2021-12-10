7ffd48fdaf597652138932ab04b637a739123b1b
sawady <sawady.faso@gmail.com>
2021-12-10 17:53:21 -0300

    chore: db health check

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { TanksModule } from './tanks/tanks.module';
 import { DatabaseModule } from './database.module';
 import { PlantsModule } from './plants/plants.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [DatabaseModule, TanksModule, PlantsModule],
-  controllers: [AppController],
+  imports: [DatabaseModule, TerminusModule, TanksModule, PlantsModule],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule {}
