ca3c26afa485f149bc2a6ce45315d969d639ee86
JonMaia <jonathannicolas.maia@gmail.com>
2021-12-18 20:40:03 -0300

    #Tanks-Se integra la vista del listado con la API

--- frontend/src/routes/opds/admin/tanks.svelte
+++ frontend/src/routes/opds/admin/tanks.svelte
@@ -1,1 +1,51 @@
+<script>
+	import Table from '../../../lib/components/table.svelte';
+	import Pagination from '../../../lib/components/pagination.svelte';
+	import { onMount } from 'svelte';
+
+    let items;
+    onMount(async () => {
+			await fetch(`http://localhost:3001/tanks`)
+			.then(r => r.json())
+			.then(data => {
+				items = data.items;
+			});
+    })
+
+	const columns = [
+		{
+			title: 'Serial Number',
+			field: 'serialNumber'
+		},
+		{
+			title: 'Lot',
+			field: 'lot'
+		},
+		{
+			title: 'Manufacturer',
+			field: 'manufacturer'
+		},
+		{
+			title: 'Gas Type',
+			field: 'gasType'
+		}
+	];
+
+	function onNext(page) {
+		console.log('despues ' + page);
+	}
+
+	function onPrev(page) {
+		console.log('antes ' + page);
+	}
+</script>
+
 <h1>Tanks</h1>
+
+<div class="header">
+	<button class="btn btn-primary">Add tank</button>
+</div>
+
+<Table {columns} {items} />
+
+<Pagination {onNext} {onPrev} />
